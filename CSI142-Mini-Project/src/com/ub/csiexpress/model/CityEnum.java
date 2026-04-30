f8a320ecac5276aaed9db065d08f964918b874e3
Claude Manwedi <79464288+Manwedi@users.noreply.github.com>
2026-04-30 15:45:33 +0200

    Created an enumeration (CityEnum) for city constants

---
+++ CSI142-Mini-Project/src/com/ub/csiexpress/model/CityEnum.java
@@ -0,0 +1,15 @@
+package com.ub.csiexpress.model;
+
+public CityEnum {
+  Kasane,
+    Francistown,
+  Orapa,
+  Maun,
+  Letlhakane,
+  Serowe,
+  Palapye,
+  Mahalapye,
+  Ghanzi,
+  Serowe,
+  Gaborone
+}
