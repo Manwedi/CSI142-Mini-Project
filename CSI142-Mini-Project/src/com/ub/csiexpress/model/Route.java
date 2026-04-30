41525b90c89118ed5ee1bb5cb44b14fcfc7f0643
Claude Manwedi <79464288+Manwedi@users.noreply.github.com>
2026-04-30 16:50:19 +0200

    Added constructor to Route class

--- CSI142-Mini-Project/src/com/ub/csiexpress/model/Route.java
+++ CSI142-Mini-Project/src/com/ub/csiexpress/model/Route.java
@@ -5,5 +5,19 @@
 package com.ub.csiexpress.model;
 
 public class Route {
-  
+  private String destination;
+  private double distanceKm;
+  private double basePrice;
+  private int passengers;
+
+  public Route(String destination, double distanceKm, double basePrice, int passengers) {
+      this.destination = destination;
+      this.distanceKm = distanceKm;
+      this.basePrice = basePrice;
+      this.passengers = passengers;
+    }
+  @Override
+  public String toString() {
+    return destination + " (" + distanceKm + "km) - P" + basePrice;
+  }
 }
