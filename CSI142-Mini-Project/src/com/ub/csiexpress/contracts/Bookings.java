023a8f9213df5ae4579b745e7e82a77d54b7692d
Claude Manwedi <79464288+Manwedi@users.noreply.github.com>
2026-04-30 16:33:50 +0200

    Fix formatting issues in Bookings.java

--- CSI142-Mini-Project/src/com/ub/csiexpress/contracts/Bookings.java
+++ CSI142-Mini-Project/src/com/ub/csiexpress/contracts/Bookings.java
@@ -17,7 +17,7 @@ public class Bookings {
     private String busName;
     private int fare;
 
-    public Bookings(String name, CityEnum from, CityEnum to,String busName, int fare) {
+    public Bookings(String name, CityEnum from, CityEnum to, String busName, int fare) {
         this.bookingsid = idCounter++;
         this.name = name;
         this.from = from;
